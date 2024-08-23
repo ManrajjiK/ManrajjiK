1749699d937c4d2a3b46e1061ace6bc118b6eeae
Manraj Kewat <159870244+ManrajjiK@users.noreply.github.com>
2024-08-23 13:08:11 +0530

    fist lecture

---
+++ 1st_L.py
@@ -0,0 +1,34 @@
+print("hello world!")
+print("sum is: ",32+4) 
+name="Manraj"
+age="21"
+print("my name is :",name,age)
+print(type(name))
+umr=3
+old= True
+a= None
+print(type(a))
+print(type(umr))
+#key words :
+a=1
+b=2
+sum=a+b
+print(sum)
+print(2*name)
+print(1.5//2) # // integer division
+print(1.5/2) 
+
+name = input("Name : ")
+age=int(input("Age : "))
+price=float(input("Price : "))
+print("my name is",name,"and iam" ,age,"yaers older") 
+print(2**3)
+#conditional staement
+
+if age>18:
+    print("you are eligible for voting")
+elif age == 18:
+    print("you are just eligible for voting")
+else:
+    print("you are not eligible for voting")
+
